17b6a33b91cafd1eb3ed558060a97e537fbc6293
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-17 03:08:04 +0530

    NoteController: Add method to fetch notes from folder

--- src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/NoteController.kt
+++ src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/NoteController.kt
@@ -1,7 +1,34 @@
 package com.oddlyspaced.rdbms.notes.controller
 
+import com.oddlyspaced.rdbms.notes.db.DbConnection
+import com.oddlyspaced.rdbms.notes.entity.Note
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class NoteController {
+
+    @GetMapping("/note/get")
+    fun getNotesFromFolder(@RequestParam(value = "folderId", required = false) folderId: Int?) = fetchNotesFromFolder(folderId?: 1)
+
+    private fun fetchNotesFromFolder(folderId: Int): MutableList<Note> {
+        val notes = mutableListOf<Note>()
+        try {
+            val statement = DbConnection.connection.createStatement()
+            val resultSet = statement.executeQuery("SELECT * FROM Note WHERE InFolder = $folderId;")
+            while (resultSet.next()) {
+                notes.add(
+                    Note(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                    )
+                )
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return notes
+    }
 }
