062685c16f2c2f4ff15430bed76454566100ac96
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-17 02:41:38 +0530

    FolderController: Add mapping and method to add folder to db

--- src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
+++ src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
@@ -2,7 +2,9 @@ package com.oddlyspaced.rdbms.notes.controller
 
 import com.oddlyspaced.rdbms.notes.db.DbConnection
 import com.oddlyspaced.rdbms.notes.entity.Folder
+import com.oddlyspaced.rdbms.notes.entity.Response
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,6 +12,9 @@ class FolderController {
 
     @GetMapping("/folders/all")
     fun getFolders() = readFolders()
+
+    @GetMapping("/folder/add")
+    fun addFolder(@RequestParam(value = "name") name: String) = addFolderToDb(name)
 
     private fun readFolders(): List<Folder> {
         val folders = mutableListOf<Folder>()
@@ -29,6 +34,19 @@ class FolderController {
             e.printStackTrace()
         }
         return folders
+    }
+
+    private fun addFolderToDb(folderName: String): Response {
+        return try {
+            println(folderName)
+            val statement = DbConnection.connection.createStatement()
+            val resultSet = statement.execute("INSERT INTO Folder (Title) VALUES('${folderName}');")
+            println(resultSet)
+            Response("Folder added successfully")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Response("Unable to add folder!")
+        }
     }
 
 }
