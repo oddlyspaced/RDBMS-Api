5ab5b7d4f2d4ed9b9f18dd3b6204cc11f37aa633
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-17 03:53:16 +0530

    NoteController: Add method to add note to db

--- src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/NoteController.kt
+++ src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/NoteController.kt
@@ -2,6 +2,7 @@ package com.oddlyspaced.rdbms.notes.controller
 
 import com.oddlyspaced.rdbms.notes.db.DbConnection
 import com.oddlyspaced.rdbms.notes.entity.Note
+import com.oddlyspaced.rdbms.notes.entity.Response
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,12 @@ class NoteController {
 
     @GetMapping("/note/get")
     fun getNotesFromFolder(@RequestParam(value = "folderId", required = false) folderId: Int?) = fetchNotesFromFolder(folderId?: 1)
+
+    @GetMapping("/note/add")
+    fun addNote(
+        @RequestParam(value = "title") title: String,
+        @RequestParam(value = "folderId", required = false) folderId: Int?
+    ) = addNoteToDb(title, folderId?: 1)
 
     private fun fetchNotesFromFolder(folderId: Int): MutableList<Note> {
         val notes = mutableListOf<Note>()
@@ -30,5 +37,21 @@ class NoteController {
             e.printStackTrace()
         }
         return notes
+    }
+
+    private fun addNoteToDb(noteTitle: String, folderId: Int): Response {
+        return try {
+            val statement = DbConnection.connection.createStatement()
+            if (FolderController().checkFolderExistsInDb(folderId).message == "Exists") {
+                statement.execute("INSERT INTO Note (Title, InFolder) VALUES('$noteTitle', $folderId);")
+                Response("Note added successfully")
+            }
+            else {
+                Response("Folder does not exist for note!")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Response("Unable to add Note!")
+        }
     }
 }
