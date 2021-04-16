b0210d395bf4c6e1cf211dcc525da3923a7f66a7
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-16 23:47:59 +0530

    controller, FolderController: Setup base class

---
+++ src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
@@ -0,0 +1,34 @@
+package com.oddlyspaced.rdbms.notes.controller
+
+import com.oddlyspaced.rdbms.notes.db.DbConnection
+import com.oddlyspaced.rdbms.notes.entity.Folder
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FolderController {
+
+    @GetMapping("/folders/all")
+    fun getFolders() = readFolders()
+
+    private fun readFolders(): List<Folder> {
+        val folders = mutableListOf<Folder>()
+        try {
+            val statement = DbConnection.connection.createStatement()
+            val resultSet = statement.executeQuery("SELECT * FROM Folder;")
+            while (resultSet.next()) {
+                folders.add(
+                    Folder(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                    )
+                )
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return folders
+    }
+
+}
