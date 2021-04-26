bfe43c1f58fcaa43e45867e1156215d5d0891863
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-27 00:44:04 +0530

    FolderController: Add method to read notes in a folder

--- src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
+++ src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
@@ -3,7 +3,11 @@ package com.oddlyspaced.rdbms.notes.controller
 import com.google.gson.Gson
 import com.oddlyspaced.rdbms.notes.db.DbConnection
 import com.oddlyspaced.rdbms.notes.entity.Folder
+import com.oddlyspaced.rdbms.notes.entity.Item
+import com.oddlyspaced.rdbms.notes.entity.Note
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -32,6 +36,56 @@ class FolderController {
             e.printStackTrace()
         }
         return folders
+    }
+
+    @GetMapping("/folder/notes")
+    fun getNotesInFolders(
+        @RequestParam(value = "folderId", required = false) folderId: Int?
+    ): String = gson.toJson(readNotesInFolder(folderId ?: 1))
+
+    private fun readNotesInFolder(folderId: Int): MutableList<Note> {
+        val notes = mutableListOf<Note>()
+        //SELECT * FROM Note WHERE InFolder = 1
+        try {
+            val statement = DbConnection.connection.createStatement()
+            val resultSet = statement.executeQuery("SELECT * FROM Note WHERE InFolder = $folderId")
+            while (resultSet.next()) {
+                notes.add(
+                    Note(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        readContentOfNote(resultSet.getInt(1))
+                    )
+                )
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return notes
+    }
+
+    private fun readContentOfNote(noteId: Int): MutableList<Item> {
+        val items = mutableListOf<Item>()
+        // SELECT * FROM Item WHERE InNote = 1
+        try {
+            val statement = DbConnection.connection.createStatement()
+            val resultSet = statement.executeQuery("SELECT * FROM Item WHERE InNote = $noteId")
+            while (resultSet.next()) {
+                items.add(
+                    Item(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getBoolean(3)
+                    )
+                )
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return items
     }
 
 
