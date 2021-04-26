89cf6a1c0fcd2d3cf482a9a3831bee101e26d92e
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-27 03:20:24 +0530

    FolderController: Add method to add fresh note in db after checking

--- src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
+++ src/main/kotlin/com/oddlyspaced/rdbms/notes/controller/FolderController.kt
@@ -94,10 +94,15 @@ class FolderController {
     @PostMapping("/note/update")
     fun updateNote(
         @RequestParam(value = "note") noteJson: String,
+        @RequestParam(value = "folderId", required = false) folderId: Int?
     ): Response {
         val type = object: TypeToken<Note>(){}.type
         val note: Note = gson.fromJson(noteJson, type)
-        return updateNoteInDb(note)
+        return if (note.id == -1) {
+            addNoteInDb(note, folderId ?: 1)
+        } else {
+            updateNoteInDb(note)
+        }
     }
 
     private fun updateNoteInDb(note: Note): Response {
@@ -110,6 +115,47 @@ class FolderController {
             return Response("Update Unsuccessful")
         }
         return Response("Updated Successfully ${note.title}")
+    }
+
+    private fun addNoteInDb(note: Note, folderId: Int): Response {
+        try {
+            val statement = DbConnection.connection.createStatement()
+            // INSERT INTO Note(Title, Date, InFolder) VALUES('Test Note', '202006191840', 1)
+            statement.execute("INSERT INTO Note(Title, Date, InFolder) VALUES('${note.title}', '${note.date}', $folderId);")
+            saveContentOfNote(note.content, searchNote(note))
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+            return Response("Add Unsuccessful")
+        }
+        return Response("Added Successfully ${note.title}")
+    }
+
+    private fun searchNote(note: Note): Int {
+        try {
+            val statement = DbConnection.connection.createStatement()
+            val resultSet = statement.executeQuery("SELECT NoteID FROM Note WHERE Title='${note.title}' AND Date='${note.date}';")
+            while (resultSet.next()) {
+                return resultSet.getInt(1)
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return -1
+    }
+
+    private fun saveContentOfNote(items: List<Item>, noteId: Int) {
+        // INSERT INTO Item(Content, IsDone, InNote) VALUES('Test Content', FALSE, 1);
+        try {
+            items.forEach { item ->
+                val statement = DbConnection.connection.createStatement()
+                statement.execute("INSERT INTO Item(Content, IsDone, InNote) VALUES('${item.content}', ${item.isDone}, $noteId);")
+            }
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 
